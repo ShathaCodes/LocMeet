5d4ca1b66701c6ccb985f474488b2edd1ac52e47
ShathaCodes <chadhasiala@insat.u-carthage.tn>
2021-03-26 21:41:11 +0100

    Added get interests in app

--- frontend/lib/api_provider.dart
+++ frontend/lib/api_provider.dart
@@ -3,13 +3,28 @@ import 'dart:io';
 import 'dart:convert';
 import 'dart:async';
 
+import 'models/Interest.dart';
+
 class ApiProvider {
-  static const addr = "your ip address";
+  static const ip = "192.168.1.4";
 
   ApiProvider();
 
-  Future<http.Response> doLogin(
-      String nickname, String password, String ip) async {
+  Future<List> getInterests() async {
+    String _url = 'http://$ip:3000/interests';
+
+    http.Response result = await http.get(_url);
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(result.body);
+      final interestsMap = jsonResponse['results'];
+      List interests = interestsMap.map((i) => Interest.fromJson(i)).toList();
+      return interests;
+    } else {
+      return null;
+    }
+  }
+
+  Future<http.Response> doLogin(String nickname, String password) async {
     String _url = 'http://$ip:3000/login';
 
     var body = {"nickname": nickname, "password": password};
@@ -18,14 +33,22 @@ class ApiProvider {
   }
 
   Future<http.Response> doRegistration(
-      String nickname, String password, String ip) async {
-    String _url = 'http://$ip:3000/register';
+    String nickname,
+    String password,
+  ) async {
+    var interests = [1, 0, 2];
 
+    Map<String, dynamic> toJson(i) => {"id": interests[i]};
+    var json = toJson(0);
+
+    String _url = 'http://$ip:3000/register';
     var body = {
-      "nickname": nickname,
-      "password": password,
-      'location': "",
-      "isAdmin": "false"
+      "user": {
+        "nickname": nickname,
+        "password": password,
+        "location": "",
+        "isAdmin": "false"
+      },
     };
 
     return http.post(_url, body: body);
